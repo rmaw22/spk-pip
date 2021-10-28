4d12639e73903cdade76a8fb62029bba1529ae15
rmaw22 <rizkymilanalpasya@gmail.com>
2021-10-28 22:22:59 +0700

    edit result NK dan NS

--- app/Http/Controllers/HasilController.php
+++ app/Http/Controllers/HasilController.php
@@ -181,8 +181,8 @@ class HasilController extends Controller
                     f.NCF,
                     f.NSF,
                     f.aspek,
-                    (CASE WHEN f.id_aspek=1 THEN (((0.6*f.NCF)+(0.4*f.NSF))/2) END) as N_K,
-                    (CASE WHEN f.id_aspek=2 THEN (((0.6*f.NCF)+(0.4*f.NSF))/2) END) as N_S                                              
+                    (CASE WHEN f.id_aspek=1 THEN (((0.6*f.NCF)+(0.4*f.NSF))) END) as N_K,
+                    (CASE WHEN f.id_aspek=2 THEN (((0.6*f.NCF)+(0.4*f.NSF))) END) as N_S                                              
                   FROM
                   (SELECT 
                         b.nis,
@@ -217,8 +217,8 @@ class HasilController extends Controller
                                   f.aspek,
                             f.id_aspek,
                             f.persen,
-                                  (CASE WHEN f.id_aspek=1 THEN (((0.6*f.NCF)+(0.4*f.NSF))/2) END) as N_K,
-                                  (CASE WHEN f.id_aspek=2 THEN (((0.6*f.NCF)+(0.4*f.NSF))/2) END) as N_S                                              
+                                  (CASE WHEN f.id_aspek=1 THEN (((0.6*f.NCF)+(0.4*f.NSF))) END) as N_K,
+                                  (CASE WHEN f.id_aspek=2 THEN (((0.6*f.NCF)+(0.4*f.NSF))) END) as N_S                                              
                                 FROM
                                 (SELECT 
                                       b.nis,
@@ -369,8 +369,8 @@ f.nama,
 f.NCF,
 f.NSF,
 f.aspek,
-(CASE WHEN f.id_aspek=1 THEN (((0.6*f.NCF)+(0.4*f.NSF))/2) END) as N_K,
-(CASE WHEN f.id_aspek=2 THEN (((0.6*f.NCF)+(0.4*f.NSF))/2) END) as N_S                                              
+(CASE WHEN f.id_aspek=1 THEN (((0.6*f.NCF)+(0.4*f.NSF))) END) as N_K,
+(CASE WHEN f.id_aspek=2 THEN (((0.6*f.NCF)+(0.4*f.NSF))) END) as N_S                                              
 FROM
 (SELECT 
     b.nis,
@@ -405,8 +405,8 @@ $nilai_rangking = DB::SELECT('SELECT
               f.aspek,
         f.id_aspek,
         f.persen,
-              (CASE WHEN f.id_aspek=1 THEN (((0.6*f.NCF)+(0.4*f.NSF))/2) END) as N_K,
-              (CASE WHEN f.id_aspek=2 THEN (((0.6*f.NCF)+(0.4*f.NSF))/2) END) as N_S                                              
+              (CASE WHEN f.id_aspek=1 THEN (((0.6*f.NCF)+(0.4*f.NSF))) END) as N_K,
+              (CASE WHEN f.id_aspek=2 THEN (((0.6*f.NCF)+(0.4*f.NSF))) END) as N_S                                              
             FROM
             (SELECT 
                   b.nis,
